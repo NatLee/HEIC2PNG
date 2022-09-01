fd5409e6e12705c3b92b318ab1dfc5ff19f6106a
NatLee <natlee.work@gmail.com>
2022-09-01 23:39:20 +0800

    [update] add py version

--- setup.py
+++ setup.py
@@ -29,13 +29,14 @@ setuptools.setup(
         'Topic :: Software Development :: Build Tools',
 
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3 :: Only',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.8',
+    python_requires='>=3.7',
     install_requires=['pillow', 'pillow-heif'],
     extras_require={
         'dev': ['check-manifest'],
